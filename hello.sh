d793b1d57a781e074881772ad53cfb9eeb23775d
hobita <korbirayen45@gmail.com>
2025-08-05 11:48:24 +0100

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo " Hello from $(hostname)"
-echo " Date: $(date)"
+echo "Hello from $(hostname)"
+echo "Date: $(date)"
+echo "Done."  # Add an extra line to make sure something shows
