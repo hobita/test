96ba1f205569603e5b1ca97f3cfd6f84f9a53ae1
hobita <korbirayen45@gmail.com>
2025-09-01 13:31:54 +0100

    Update TEST_FILE.sh

--- TEST_FILE.sh
+++ TEST_FILE.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-Nom du fichier à créer
+#Nom du fichier à créer
 TEST_FILE="/tmp/test_installation.txt"
 
-Message à écrire dans le fichier
+#Message à écrire dans le fichier
 echo "L'installation de ce script a réussi le $(date)." > "$TEST_FILE"
 
-Afficher un message de succès
+#Afficher un message de succès
 echo "Fichier de test créé avec succès à l'emplacement : $TEST_FILE"
